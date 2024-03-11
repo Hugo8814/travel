711b1d07b9dc9d5ba5ad5bfbd858038ea9540bc5
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-03-11 20:50:30 +0000

    coooili

--- src/App.js
+++ src/App.js
@@ -20,13 +20,24 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
+    if (!description || !quantity) return;
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false,
+    };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
   return (
-    <form className="add-form" onSumbit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for your trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
